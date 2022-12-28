bed42894cdc917814832c4bfb0a9b3a2fdfd8359
Jograj singh <89298550+Jukes312@users.noreply.github.com>
2022-12-28 18:39:17 +0000

    Build ready

---
+++ build/precache-manifest.4fad4c17e547c588dd0b67febdc2a2a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363448d70a9eda44901ef0e3f550a396",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f240d351c560104aadf3",
+    "url": "/static/css/2.499f5c06.chunk.css"
+  },
+  {
+    "revision": "df7f6f1d97a0543b8b89",
+    "url": "/static/css/main.5b130ed7.chunk.css"
+  },
+  {
+    "revision": "f240d351c560104aadf3",
+    "url": "/static/js/2.ca064318.chunk.js"
+  },
+  {
+    "revision": "df7f6f1d97a0543b8b89",
+    "url": "/static/js/main.eb49e189.chunk.js"
+  },
+  {
+    "revision": "aeaf5c606a64ce9d6b4c",
+    "url": "/static/js/runtime~main.ac5a3147.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
